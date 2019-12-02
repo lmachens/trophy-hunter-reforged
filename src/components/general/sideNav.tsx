c1d53802aa53969a9a1582d9063e19110acbd03b
lmachens <leon.machens@googlemail.com>
2019-12-02 20:06:08 +0100

    Remove links

--- src/components/general/sideNav.tsx
+++ src/components/general/sideNav.tsx
@@ -4,9 +4,9 @@ import { useRouter } from "next/router";
 import GamesNavItem from "./gamesNavItem";
 
 const links = [
-  { href: "/lol", alt: "League of Legends", src: "/images/lol.png" },
-  { href: "/pubg", alt: "PUBG", src: "/images/pubg.png" },
-  { href: "/fortnite", alt: "Fortnite", src: "/images/fortnite.png" }
+  { href: "/", alt: "League of Legends", src: "/images/lol.png" },
+  { href: "/", alt: "PUBG", src: "/images/pubg.png" },
+  { href: "/", alt: "Fortnite", src: "/images/fortnite.png" }
 ];
 
 const SideNav: React.FC = () => {
